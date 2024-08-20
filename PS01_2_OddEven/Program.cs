c40d097e3b0f499d0770a60fa0f94fa54a80a493
lawince <106462896+Netoerare@users.noreply.github.com>
2024-08-20 19:35:58 +0800

    Update Program.cs

--- PS01_2_OddEven/Program.cs
+++ PS01_2_OddEven/Program.cs
@@ -8,19 +8,15 @@ namespace PS01_2_OddEven
 {
     class Program
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
             Console.Write("Enter a number: ");
             int number = Convert.ToInt32(Console.ReadLine());
-    
-            if (number > 0)
-                {
-                    Console.WriteLine($"The number {number} is positive.");
-                }
-            else
-                {
-                    Console.WriteLine($"The number {number} is negative.");
-                }
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"The number {number} is even.");
+            }
+        
         }
     }
 }
