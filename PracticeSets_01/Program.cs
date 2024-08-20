03e0ea5f39f5e152f81b534e11842c1bb31423fd
lawince <106462896+Netoerare@users.noreply.github.com>
2024-08-20 18:23:07 +0800

    Update Program.cs

--- PracticeSets_01/Program.cs
+++ PracticeSets_01/Program.cs
@@ -10,6 +10,12 @@ namespace PracticeSets_01
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"The number {number} is even.");
+            }
         }
     }
 }
