8b362a42d7863be402cd5c1c5f73e436d08831c0
lawince <106462896+Netoerare@users.noreply.github.com>
2024-08-20 19:01:13 +0800

    Update Program.cs

--- PS01_5_SwitchDays/Program.cs
+++ PS01_5_SwitchDays/Program.cs
@@ -10,6 +10,30 @@ namespace PS01_5_SwitchDays
     {
         static void Main(string[] args)
         {
+        const double Pi = 3.14;
+
+        Console.Write("Enter your choice (A for area, P for perimeter, X to exit): ");
+        char choice = Convert.ToChar(Console.ReadLine().ToUpper());
+
+        Console.Write("Enter the radius of the circle: ");
+        int radius = Convert.ToInt32(Console.ReadLine());
+
+        switch (choice)
+            {
+                case 'A':
+                    int area = (int)(Pi * radius * radius);
+                    Console.WriteLine($"The area of the circle is: {area}");
+                    break;
+    
+                case 'P':
+                    int perimeter = (int)(2 * Pi * radius);
+                    Console.WriteLine($"The perimeter of the circle is: {perimeter}");
+                    break;
+    
+                case 'X':
+                    Console.WriteLine("Exiting the application.");
+                    break;
+            }
         }
     }
 }
